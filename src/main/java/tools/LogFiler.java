a02ab085ef3c927100aebbebab3769e521b1a872
Roman Pružinský <xpruzir00@stud.fit.vut.cz>
2026-04-28 05:53:58 +0200

    Simplified logs

--- src/main/java/tools/LogFiler.java
+++ src/main/java/tools/LogFiler.java
@@ -1,7 +1,6 @@
 package tools;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -13,105 +12,77 @@ import event.*;
 import gamer.Player;
 
 public class LogFiler {
-	public record ReplayHeader(AvailableMaps.MapMetadata map, List<String> playerNames) {}
+	private static final String MAP_KEY = "map";
+	private static final String PLAYERS_KEY = "players";
+	private static final String EVENTS_KEY = "events";
+	private static final String TYPE_KEY = "type";
+
+	public record ReplayHeader(AvailableMaps.MapMetadata map, List<String> playerNames) {
+	}
 
 	private static Gson buildGson(List<Player> players) {
 		return new GsonBuilder()
 				.setPrettyPrinting()
-				.registerTypeAdapter(GameEvent.class, new GameEventAdapter())
-				.registerTypeAdapter(Player.class, new PlayerAdapter(players))
+				.registerTypeAdapter(GameEvent.class,
+						(JsonSerializer<GameEvent>) (event, type, ctx) -> {
+							JsonObject obj = ctx.serialize(event, event.getClass()).getAsJsonObject();
+							obj.addProperty(TYPE_KEY, event.type().name());
+							return obj;
+						})
+				.registerTypeAdapter(GameEvent.class,
+						(JsonDeserializer<GameEvent>) (json, type, ctx) -> {
+							JsonObject obj = json.getAsJsonObject();
+							return switch (GameEventType.valueOf(obj.get(TYPE_KEY).getAsString())) {
+								case UNIT_MOVED -> ctx.deserialize(obj, UnitMovedEvent.class);
+								case UNIT_ATTACKED -> ctx.deserialize(obj, UnitAttackEvent.class);
+								case UNIT_DIED -> ctx.deserialize(obj, UnitDiedEvent.class);
+								case UNIT_BOUGHT -> ctx.deserialize(obj, UnitBoughtEvent.class);
+								case CITY_CAPTURED -> ctx.deserialize(obj, CityCapturedEvent.class);
+								case CAPTURE_PROGRESS -> ctx.deserialize(obj, CaptureProgressEvent.class);
+								case TURN_CHANGED -> ctx.deserialize(obj, TurnChangedEvent.class);
+								case PLAYER_ELIMINATED -> ctx.deserialize(obj, MultipleGameEvent.class);
+							};
+						})
+				.registerTypeAdapter(Player.class,
+						(JsonSerializer<Player>) (player, type,
+								ctx) -> new JsonPrimitive(player.getName()))
+				.registerTypeAdapter(Player.class,
+						(JsonDeserializer<Player>) (json, type, ctx) -> players.stream()
+								.filter(p -> p.getName().equals(json.getAsString()))
+								.findFirst()
+								.orElse(null))
 				.create();
 	}
 
-	public static void save(List<GameEvent> eventLog, Path path, AvailableMaps.MapMetadata map, List<Player> players)
-			throws IOException {
+	public static void save(List<GameEvent> eventLog, Path path, AvailableMaps.MapMetadata map,
+			List<Player> players) throws IOException {
 		Gson gson = buildGson(players);
 		JsonObject root = new JsonObject();
-		root.addProperty("map", map.fileprefix());
-		JsonArray playerArray = new JsonArray();
-		players.forEach(player -> playerArray.add(player.getName()));
-		root.add("players", playerArray);
-		root.add("events", gson.toJsonTree(eventLog, new TypeToken<List<GameEvent>>() {}.getType()));
+		root.addProperty(MAP_KEY, map.fileprefix());
+		root.add(PLAYERS_KEY, gson.toJsonTree(players));
+		root.add(EVENTS_KEY,
+				gson.toJsonTree(eventLog, new TypeToken<List<GameEvent>>() {}.getType()));
 		Files.writeString(path, gson.toJson(root));
 	}
 
 	public static ReplayHeader loadHeader(Path path) throws IOException {
-		JsonObject object = readRoot(path);
-		if (!object.has("map") || !object.has("players"))
+		JsonObject obj = JsonParser.parseString(Files.readString(path)).getAsJsonObject();
+		if (!obj.has(MAP_KEY) || !obj.has(PLAYERS_KEY))
 			throw new IllegalArgumentException("Replay log is missing map/player header");
 
-		AvailableMaps.MapMetadata map = AvailableMaps.findByFileprefix(object.get("map").getAsString());
 		List<String> playerNames = new ArrayList<>();
-		object.getAsJsonArray("players").forEach(element -> playerNames.add(element.getAsString()));
-		return new ReplayHeader(map, playerNames);
+		obj.getAsJsonArray(PLAYERS_KEY).forEach(e -> playerNames.add(e.getAsString()));
+		return new ReplayHeader(AvailableMaps.findByFileprefix(obj.get(MAP_KEY).getAsString()),
+				playerNames);
 	}
 
 	public static List<GameEvent> loadEvents(Path path, List<Player> players) throws IOException {
-		Gson gson = buildGson(players);
-		JsonObject root = readRoot(path);
-		Type listType = new TypeToken<List<GameEvent>>() {}.getType();
-		if (!root.has("events"))
+		JsonObject obj = JsonParser.parseString(Files.readString(path)).getAsJsonObject();
+		if (!obj.has(EVENTS_KEY))
 			throw new IllegalArgumentException("Replay log is missing events list");
-		return gson.fromJson(root.get("events"), listType);
-	}
 
-	private static JsonObject readRoot(Path path) throws IOException {
-		String json = Files.readString(path);
-		return JsonParser.parseString(json).getAsJsonObject();
-	}
-
-	private static class GameEventAdapter
-			implements JsonSerializer<GameEvent>, JsonDeserializer<GameEvent> {
-
-		@Override
-		public JsonElement serialize(GameEvent event, Type type, JsonSerializationContext ctx) {
-			JsonObject object = new JsonObject();
-			object.addProperty("type", event.type().name());
-
-			JsonObject rest = ctx.serialize(event, event.getClass()).getAsJsonObject();
-			rest.entrySet().forEach(e -> object.add(e.getKey(), e.getValue()));
-
-			return object;
-		}
-
-		@Override
-		public GameEvent deserialize(JsonElement json, Type type, JsonDeserializationContext context)
-				throws JsonParseException {
-			JsonObject obj = json.getAsJsonObject();
-			GameEventType eventType = GameEventType.valueOf(obj.get("type").getAsString());
-			return switch (eventType) {
-				case UNIT_MOVED -> context.deserialize(obj, UnitMovedEvent.class);
-				case UNIT_ATTACKED -> context.deserialize(obj, UnitAttackEvent.class);
-				case UNIT_DIED -> context.deserialize(obj, UnitDiedEvent.class);
-				case UNIT_BOUGHT -> context.deserialize(obj, UnitBoughtEvent.class);
-				case CITY_CAPTURED -> context.deserialize(obj, CityCapturedEvent.class);
-				case CAPTURE_PROGRESS -> context.deserialize(obj, CaptureProgressEvent.class);
-				case TURN_CHANGED -> context.deserialize(obj, TurnChangedEvent.class);
-				case PLAYER_ELIMINATED -> context.deserialize(obj, MultipleGameEvent.class);
-			};
-		}
-	}
-
-
-	private static class PlayerAdapter implements JsonSerializer<Player>, JsonDeserializer<Player> {
-		private final List<Player> players;
-
-		PlayerAdapter(List<Player> players) {
-			this.players = players;
-		}
-
-		@Override
-		public JsonElement serialize(Player player, Type type, JsonSerializationContext ctx) {
-			return new JsonPrimitive(player.getName());
-		}
-
-		@Override
-		public Player deserialize(JsonElement json, Type type, JsonDeserializationContext ctx) {
-			String name = json.getAsString();
-			return players.stream()
-					.filter(player -> player.getName().equals(name))
-					.findFirst()
-					.orElse(null);
-		}
+		return buildGson(players).fromJson(
+				obj.get(EVENTS_KEY),
+				new TypeToken<List<GameEvent>>() {}.getType());
 	}
 }
