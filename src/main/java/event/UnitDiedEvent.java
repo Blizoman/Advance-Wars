3fd1e921ba282a3d45b36ebdb9d57416edfa10f6
Roman Pružinský <xpruzir00@stud.fit.vut.cz>
2026-04-28 15:00:01 +0200

    fixed UnitDied stepback

--- src/main/java/event/UnitDiedEvent.java
+++ src/main/java/event/UnitDiedEvent.java
@@ -1,18 +1,38 @@
 package event;
 
+import board.Tile;
 import game.Game;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import unit.Unit;
 
-public record UnitDiedEvent(Unit unit) implements GameEvent {
+@RequiredArgsConstructor
+public class UnitDiedEvent implements GameEvent {
+	@Getter
+	private final Unit unit;
+	private Integer captureHpBeforeReset = null;
+	private Integer hpBeforeDeath = null;
+
 	public GameEventType type() {
 		return GameEventType.UNIT_DIED;
 	}
 
 	public void execute(Game game) {
+		Tile tile = game.getGameBoard().getTile(unit.getPosition());
+		hpBeforeDeath = unit.getHp();
+		captureHpBeforeReset = null;
+		if (tile.getTerrain().isCapturable()) {
+			captureHpBeforeReset = tile.getCaptureHp();
+			tile.resetCapturableHp();
+		}
+		unit.setHp(0);
 		game.removeUnit(unit);
 	}
 
 	public void undo(Game game) {
+		unit.setHp(hpBeforeDeath);
 		game.placeUnit(unit);
+		if (captureHpBeforeReset != null)
+			game.getGameBoard().getTile(unit.getPosition()).setCaptureHp(captureHpBeforeReset);
 	}
 }
