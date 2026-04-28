d1fdfdd9c82684425de572936bdf3b29aaf50087
Roman Pružinský <xpruzir00@stud.fit.vut.cz>
2026-04-28 13:24:35 +0200

    Added maps

--- src/main/java/board/AvailableMaps.java
+++ src/main/java/board/AvailableMaps.java
@@ -15,7 +15,11 @@ public class AvailableMaps {
 	private static final List<MapMetadata> availableMaps = List.of(
 			new MapMetadata("SIMPLE", "map0", 2),
 			new MapMetadata("SIMPLE larger", "map1", 2),
-			new MapMetadata("ASYMETRIC", "map2", 3));
+			new MapMetadata("ASYMETRIC", "map2", 3),
+			new MapMetadata("BOT-ADVANTAGE", "map3", 2),
+			new MapMetadata("BOT-ADVANTAGE 2", "map_3p_hard", 3),
+			new MapMetadata("tactical", "tactical", 2)//
+	);
 
 	public static String getFilename(MapMetadata map) {
 		return map.fileprefix() + ".json";
