086d645ab603d33ec8ac2618169e68d5631bcdaf
Blizoman <xblizna00@stud.fit.vut.cz>
2026-05-05 09:21:21 +0200

    Fixed bug with maps

--- src/main/java/board/AvailableMaps.java
+++ src/main/java/board/AvailableMaps.java
@@ -26,8 +26,8 @@ public class AvailableMaps {
 			new MapMetadata("BOT - ADVANTAGE", "botAdvantage", 2),
 			new MapMetadata("2 BOTS - ADVANTAGE", "2botsAdvantage", 3),
 			new MapMetadata("tactical", "tactical", 3),
-			new MapMetadata("Crossfire (4 Players)", "maps/4p_crossfire.json", 4),
-			new MapMetadata("Twin Rivers (2 Players)", "maps/2p_twin_rivers.json", 2)//
+			new MapMetadata("Crossfire", "4p_crossfire", 4),
+			new MapMetadata("Twin Rivers", "2p_twin_rivers", 2)//
 	);
 
 	public static String getFilename(MapMetadata map) {
